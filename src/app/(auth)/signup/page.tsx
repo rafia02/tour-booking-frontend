aabb3f52d36d6fa32e8621cd0c2644068a37e753
rafia02 <rafiabinterashed01@gmail.com>
2026-06-13 15:56:15 +0600

    updated

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,11 +1,10 @@
 import Signup from "@/components/Auth/Signup";
-import React from "react";
 
 const SignUpPage = () => {
   return (
-    <div>
+    <>
       <Signup />
-    </div>
+    </>
   );
 };
 
